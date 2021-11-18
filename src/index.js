151f6cfc9525b248ba695d8ab1c63f4618a27a51
Gary Holmes <gaz2050@msn.com>
2021-11-18 21:39:40 +0000

    react initial build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ul>
+    <p>calculator</p>
+    <p>pace converter</p>
+    <p>Logon Database</p>
+    <p>Stopwatch</p>
+    <p>Rock paper scissors</p>
+  </ul>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
